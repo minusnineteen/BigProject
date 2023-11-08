fccdffc992d55d294233f68252b4581ee8f00144
minusnineteen <kidfantonmin@gmail.com>
2023-11-08 17:48:36 +0700

    id

--- personal.php
+++ personal.php
@@ -42,89 +42,91 @@ if(isset($_POST['del'])) {
     include('incl/header.php');
     ?>
     <section id="save">
-        <?php
-        $current_page = isset($_GET['page']) ? $_GET['page'] : 1;
-        $item_per_page = isset($_GET['per_page']) ? $_GET['per_page'] : 10;
-        $offset = ($current_page - 1) * $item_per_page;
-        $id = isset($_GET['phone_number']) ? $_GET['phone_number'] : $_SESSION['id'];
-        $sql_info = mysqli_query($con, "select * from tbl_information where phone_number = " . $id . 
-            " limit " . $item_per_page . " offset " . $offset);
-        while($row_info = mysqli_fetch_array($sql_info)) {
-        ?>
-        <div class='save row'>
-            <div class="save-left">
-                <a href="post.php?id=<?php echo $row_info['information_code'] ?>"><img src="up/<?php echo $row_info['picture'] ?>" width="100px" height="100px"></a>
+        <div class="bg">
+            <?php
+            $current_page = isset($_GET['page']) ? $_GET['page'] : 1;
+            $item_per_page = isset($_GET['per_page']) ? $_GET['per_page'] : 10;
+            $offset = ($current_page - 1) * $item_per_page;
+            $id = isset($_GET['phone_number']) ? $_GET['phone_number'] : $_SESSION['id'];
+            $sql_info = mysqli_query($con, "select * from tbl_information where phone_number = " . $id . 
+                " limit " . $item_per_page . " offset " . $offset);
+            while($row_info = mysqli_fetch_array($sql_info)) {
+            ?>
+            <div class='save row'>
+                <div class="save-left">
+                    <a href="post.php?id=<?php echo $row_info['information_code'] ?>"><img src="up/<?php echo $row_info['picture'] ?>" width="100px" height="100px"></a>
+                </div>
+                <div class="save-right">
+                    <div class="save-right-top">
+                        <p><?php echo $row_info['title']?></p>
+                        <p><?php echo $row_info['acreage']?> m<sup>2</sup> - <?php echo $row_info['room']?> phòng</p>
+                        <p><?php echo $row_info['price']?> tỷ</p>
+                    </div>
+                    <div class="save-right-bottom">
+                        <p></p>
+                        <form method="POST">
+                            <input type="hidden" name="information_code" value="<?php echo $row_info['information_code'] ?>">
+                            <?php
+                            if($_SESSION['id'] == $_GET['phone_number'])
+                            {
+                            ?>
+                            <a href="http://localhost/land/edit.php?information_code=<?php echo $row_info['information_code'] ?>" title="Sửa"><i class='bx bx-edit'></i></a>
+                            <input type="submit" name="del" value="Xóa">
+                            <?php
+                            }
+                            ?>
+                            <input type="submit" name="save" value="Lưu">
+                        </form>
+                    </div>
+                </div>
             </div>
-            <div class="save-right">
-                <div class="save-right-top">
-                    <p><?php echo $row_info['title']?></p>
-                    <p><?php echo $row_info['acreage']?> m<sup>2</sup> - <?php echo $row_info['room']?> phòng</p>
-                    <p><?php echo $row_info['price']?> tỷ</p>
-                </div>
-                <div class="save-right-bottom">
-                    <p></p>
-                    <form method="POST">
-                        <input type="hidden" name="information_code" value="<?php echo $row_info['information_code'] ?>">
-                        <?php
-                        if($_SESSION['id'] == $_GET['phone_number'])
-                        {
-                        ?>
-                        <a href="http://localhost/land/edit.php?information_code=<?php echo $row_info['information_code'] ?>" title="Sửa"><i class='bx bx-edit'></i></a>
-                        <input type="submit" name="del" value="Xóa">
-                        <?php
-                        }
-                        ?>
-                        <input type="submit" name="save" value="Lưu">
-                    </form>
-                </div>
-            </div>
-        </div>
-        <?php 
-        }
-        ?>
-        <div class='paging-container'>
-            <div class='paging-wrapper'>
-                <?php
-                if($current_page != 1)
-                {
-                ?>
-                <div class='paging-item'>
-                    <a href="?phone_number=<?php echo $id?>&per_page=10&page=<?php echo ($current_page > 1) ? ($current_page - 1) : 1; ?>" class='paging-text'>
-                        <i class='bx bx-chevron-left'></i>
-                    </a>
-                </div>
-                <?php
-                }
-                ?> 
-                <?php
-                $phone_number = isset($_GET['phone_number']) ? $_GET['phone_number'] : null;
-                $sql_info = mysqli_query($con, "select count(*) as total_rows from tbl_information where phone_number = " . $phone_number);
-                $row = mysqli_fetch_assoc($sql_info);
-                $total_rows = $row['total_rows'];
-                $page_number = $total_rows/10;
-                if($total_rows % 10 != 0) {
-                    $page_number += 1;
-                }
-                $i = 1;
-                while($i <= $page_number) {  
-                ?>
-                <div class='paging-item'><a href="?phone_number=<?php echo $id?>&per_page=10&page=<?php echo $i; ?>" class='paging-text'><?php echo $i ?></a></div>
+            <?php 
+            }
+            ?>
+            <div class='paging-container'>
+                <div class='paging-wrapper'>
                     <?php
-                    $i++;
-                    }
-                    ?>
-                    <?php
-                    if($current_page < ($page_number - 1)) {
+                    if($current_page != 1)
+                    {
                     ?>
                     <div class='paging-item'>
-                        <a href="?phone_number=<?php echo $id?>&per_page=10&page=<?php echo ($current_page < $page_number) ? ($current_page + 1) : $current_page; ?>" class='paging-text'>
-                            <i class='bx bx-chevron-right'></i>
+                        <a href="?phone_number=<?php echo $id?>&per_page=10&page=<?php echo ($current_page > 1) ? ($current_page - 1) : 1; ?>" class='paging-text'>
+                            <i class='bx bx-chevron-left'></i>
                         </a>
                     </div>
                     <?php
                     }
-                    $_SESSION['area_id'] = 0;
-                    ?>            
+                    ?> 
+                    <?php
+                    $phone_number = isset($_GET['phone_number']) ? $_GET['phone_number'] : null;
+                    $sql_info = mysqli_query($con, "select count(*) as total_rows from tbl_information where phone_number = " . $phone_number);
+                    $row = mysqli_fetch_assoc($sql_info);
+                    $total_rows = $row['total_rows'];
+                    $page_number = $total_rows/10;
+                    if($total_rows % 10 != 0) {
+                        $page_number += 1;
+                    }
+                    $i = 1;
+                    while($i <= $page_number) {  
+                    ?>
+                    <div class='paging-item'><a href="?phone_number=<?php echo $id?>&per_page=10&page=<?php echo $i; ?>" class='paging-text'><?php echo $i ?></a></div>
+                        <?php
+                        $i++;
+                        }
+                        ?>
+                        <?php
+                        if($current_page < ($page_number - 1)) {
+                        ?>
+                        <div class='paging-item'>
+                            <a href="?phone_number=<?php echo $id?>&per_page=10&page=<?php echo ($current_page < $page_number) ? ($current_page + 1) : $current_page; ?>" class='paging-text'>
+                                <i class='bx bx-chevron-right'></i>
+                            </a>
+                        </div>
+                        <?php
+                        }
+                        $_SESSION['area_id'] = 0;
+                        ?>            
+                    </div>
                 </div>
             </div>
         </div>
